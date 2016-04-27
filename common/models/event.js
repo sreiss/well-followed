f1f5534f612e78eba64db3af377c8c93b63b3f71
sreiss <simon.reiss57@gmail.com>
2016-04-28 00:49:38 +0200

    Working on event collision

--- common/models/event.js
+++ common/models/event.js
@@ -1,4 +1,55 @@
 module.exports = function(Event) {
+    /**
+     * Before saving an event, checks if an other event isn't already booked at this date and if the start and end are coherent.
+     */
+    Event.observe('before save', function(ctx, next) {
+        if (ctx.instance.start.getTime() > ctx.instance.end.getTime()) {
+            next(new Error("Start must be inferior to end."));
+        } else {
+            var eventFilter = {
+                where: {
+                    and: [
+                        {start: {lt: ctx.instance.start}},
+                        {end: {gt: ctx.instance.start}}
+                    ]
+                }
+            };
+            Event.findOne(eventFilter)
+                .then(function(event) {
+                    if (event) {
+                        next(new Error('Another event is already booked at this date.'));
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+                .then(function(hasError) {
+                    if (hasError) {
+                        var eventFilter = {
+                            where: {
+                                and: [
+                                    {start: {lt: ctx.instance.end}},
+                                    {end: {gt: ctx.instance.end}}
+                                ]
+                            }
+                        };
+                        return Event.findOne(eventFilter);
+                    } else {
+                        return false;
+                    }
+                })
+                .then(function(event) {
+                    if (!event) {
+                        next();
+                    } else {
+                        next(new Error('Another event is already booked at this date.'));
+                    }
+                })
+                .catch(function(err) {
+                    next(err);
+                });
+        }
+    });
 
     /**
      * Allows to cancel an event (without deleting it).
